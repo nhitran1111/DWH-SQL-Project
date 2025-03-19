d92845cb9b8f6af491c99fbcc6f74b45856ab47b
nhitran1111 <nhitran.da@gmail.com>
2025-03-20 03:08:37 +0700

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -3,9 +3,9 @@
 Create database and schemas
 ---------------------------------------------------------------------------------------------------------------------------------------------------------
 Scripts purpose:
-				This script creates a new database named 'DataWarehouse'. After checking, if the database named 'DataWarehouse' is already exist in the server,
-				this database is dropped and recreated. Once the new database has been successfully created, the scripts also set up three schemas within the database 
-				including 'bronze', 'silver', 'gold'
+		This script creates a new database named 'DataWarehouse'. After checking, if the database named 'DataWarehouse' is already exist in the 
+		server, this database is dropped and recreated. Once the new database has been successfully created, the scripts also set up three 
+		schemas within the database including 'bronze', 'silver', 'gold'.
 ---------------------------------------------------------------------------------------------------------------------------------------------------------
 */
  
